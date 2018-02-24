6fd368a847d00dbfee7738ad261a7affcc9b773c
Jean-Loup MONNIER <jloupm@gmail.com>
2018-02-24 20:45:12 +0100

    implémentation ping pong
    ping pong entre mobile et pc

--- Tests_unitaires/Mobile/test_ws/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ Tests_unitaires/Mobile/test_ws/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -1,26 +1,32 @@
 document.addEventListener('deviceready', function () {
-
+var ws
 }, false);
 
 function connect(){
   var ip = $("#idpeerjs").val()
-  var ws = new WebSocket('ws://'+ip);
+  ws = new WebSocket('ws://'+ip+":8080");
 
   ws.onopen = function () {
       console.log('open');
-      this.send('hello');         // transmit "hello" after connecting
+      screenlog('green','open')
+      this.send('ping');         // transmit "hello" after connecting
   };
 
   ws.onmessage = function (event) {
       console.log(event.data);    // will be "hello"
-      this.close();
+      screenlog('green',event.data)
+      if (event.data = "pong" ){
+        this.send('ping')
+      }
   };
 
   ws.onerror = function () {
+      screenlog('red','error')
       console.log('error occurred!');
   };
 
   ws.onclose = function (event) {
+      screenlog('red','close code=' + event.code)
       console.log('close code=' + event.code);
   };
 
