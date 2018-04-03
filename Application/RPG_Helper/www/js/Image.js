da8533c3e0f90e45966b6deed4945a6a8ec27f64
Jean-Loup MONNIER <jloupm@gmail.com>
2018-04-03 21:02:19 +0200

    Widget & Image :+1:
    + classe widget et image faite
    +  petite interface de test
    + ajout du logo

---
+++ Application/RPG_Helper/www/js/Image.js
@@ -0,0 +1,26 @@
+class Image extends Widget {
+  constructor(parent, classes, src, height, width) {
+    super(parent, classes)
+    this.src = src;
+    this.height = height;
+    this.width = width;
+    this.show();
+  }
+
+  show(){
+    $(this.parent).append(this.getHTML());
+    this.applyStyle();
+  }
+
+  getHTML(){
+    var id = this.isID() ? 'id="'+ this.id +'"' : '';
+    var classes = this.isClasses() ? 'class="'+ this.classes +'"' : '';
+    return '<img src="'+this.src+'" '+id+" "+classes+'>';
+  }
+
+  applyStyle(){
+    $("#"+this.id).css("height",this.height+"px");
+    $("#"+this.id).css("width",this.width+"px");
+  }
+  
+}
---
+++ Application/RPG_Helper/www/js/Widget.js
@@ -0,0 +1,32 @@
+class Widget {
+  constructor(parent, classes) {
+    this.parent = parent;
+    this.id = Widget.generate_ID();
+    this.classes = classes;
+    return this.id;
+  }
+
+  isID(){
+    if (typeof this.id == "undefined") {
+         return false;
+     }
+     return true;
+  }
+
+  isClasses(){
+    if (typeof this.classes == "undefined") {
+         return false;
+     }
+     return true;
+  }
+
+  static generate_ID() {
+
+    if( typeof Widget.counter == 'undefined' ) {
+        Widget.counter = -1;
+    }
+    Widget.counter++;
+    return Widget.counter;
+}
+
+}
