f1d8d773b7404bd85c6b2cc2e9e00a29f741f40c
Le Coz Loick <loick.lecoz@gmail.com>
2018-04-08 14:28:10 +0200

    Test + début code
    Test unitaires de BDD (qui ne marchent pas)
    Début de code des classes Character / Player / Generation

---
+++ Logiciel/Code Loick/Character.js
@@ -0,0 +1,17 @@
+
+class Character{
+	constructor(n,c,id,im){
+		this.name = n
+		this.carac = c
+		this.id = id
+		this.image = im
+
+	}
+
+	modCar(c){
+		this.carac = c;
+	}
+
+
+}
+
---
+++ Logiciel/Code Loick/Player.js
@@ -0,0 +1,11 @@
+//import ? 
+
+class Player extends Character{
+	constructor(n,c,id,im,inf){
+	super(n,c,id,im)
+	this infos = inf
+
+	//effect ?
+
+	}
+
