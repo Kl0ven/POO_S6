43ef2b7de6ffc0ecec2867f2f53c1b3acec248aa
Le Coz Loick <loick.lecoz@gmail.com>
2018-04-17 10:36:39 +0200

    Architecture finale code
    17/04/18

--- Logiciel/RPG_Helper/js/index.js
+++ Logiciel/RPG_Helper/js/index.js
@@ -2,6 +2,6 @@ $(document).ready(function()
 {
 	new PC_Button($(".app_PC_1"),"w3-button w3-black","hello",clickpopup)
 	new TextArea($(".app_PC_2"),$(".app_PC_2"),undefined,5,20,undefined,undefined)
-	new Image ($(".image"),undefined,"img/jppdejpp.png",100,100)
+	new Image ($(".image"),undefined,"assets/jppdejpp.png",100,100)
 	//les 2 app_PC : le bouton et la textarea seront affichés l'un en dessous de l'autre
 })
