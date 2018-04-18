cac13953a088bbdc865ae2168c00fabe9da80d11
Sairjean <artutur22@gmail.com>
2018-04-18 10:22:46 +0200

    Avancement ?
    Pas sur, jessaye des trucs...

--- Logiciel/RPG_Helper/js/Communication.js
+++ Logiciel/RPG_Helper/js/Communication.js
@@ -1,14 +1,27 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
 class Communication{
 	constructor(pc_app){
 		this.pc_app = pc_app;
 	}
 	openCom(pinfos,resume){
-		if (resume == 0) {}
-			while (this.pc_app.getOpenConnection()==True){
+		while (this.pc_app.getOpenConnection()==True){
+			if (resume ==0) {
 				//Fonction de connexion des téléphones
-				//Quand un téléphone se connecte
-				comm_Handler = new Com_Handler(this);
-				this.comm_Handler = comm_Handler;
+				wss.on('connection', function connection(ws) {
+					ws.send('Vous êtes connecté !');
+					ws.on('message', function incoming(message) {
+						pinfos = message; // On rentre les infos dans pinfos
+						//Quand un téléphone se connecte
+						comm_Handler = new Com_Handler(this);
+						this.comm_Handler = comm_Handler;
+						this.comm_Handler.playerConnection(pinfos,resume);
+
+					}
+				}
+				
 			}
+		}
 	}
 }
