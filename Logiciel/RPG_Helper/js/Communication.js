c0a1db8ced5bbcc2509769c9806a3559357b518c
Sairjean <artutur22@gmail.com>
2018-04-22 14:43:15 +0200

    Finalisation de la classe
    Classe modifiée et finie : la classe récupère la connexion puis crée un
    com handler qui gérera la suite.

--- Logiciel/RPG_Helper/js/Communication.js
+++ Logiciel/RPG_Helper/js/Communication.js
@@ -4,33 +4,24 @@ class Communication{
 	constructor(ip,pc_app){
 		this.pc_app = pc_app;
 		this.ip = ip;
-		this.url = 'ws://'+ip+':8080';
 		this.wss = WebSocket.Server({ port: 8080 });
+		this.comm_Handlers =[];
 	}
 	openCom(pinfos,resume){
+		that = this
 		while (this.pc_app.getOpenConnection()==True){
 			//Fonction de connexion des téléphones
 			wss.on('connection', function connection(ws) {
-				ws.send('Vous êtes connecté !');
-				if (resume ==0) { //Si la partie est nouvelle pinfos = NULL
-					ws.on('message', function incoming(message) {
-						pinfos = message; // On rentre les infos dans pinfos
-						comm_Handler = new Com_Handler(this);
-						this.comm_Handler = comm_Handler;
-						this.comm_Handler.playerConnection(pinfos,resume);//On envoie
-
-					});
-				}
-				else { //Si la partie existe déja, on crée un comm handler et on lance playerconnexion
-					comm_Handler = new Com_Handler(this);
-					this.comm_Handler = comm_Handler;
-					this.comm_Handler.playerConnection(pinfos,resume);
-				}
+						comm_Handler = new Com_Handler(that,ws); // On crée un comhandler et on lui envoie la connexion.
+						this.comm_Handlers.push(comm_Handler);
+						comm_Handler.playerConnection(pinfos,resume);//On envoie
 			});
 				
 		
 		}
 	}
+
+
 
 
 }
