bacd0a348a7a1affc11432476294cb2679529e39
Sairjean <artutur22@gmail.com>
2018-04-24 09:54:51 +0200

    Classe presque finie
    Normalement il y a tout ce qu'il faut, excepté la gestion de la
    sauvegarde de la campagne

--- Logiciel/RPG_Helper/js/Com_Handler.js
+++ Logiciel/RPG_Helper/js/Com_Handler.js
@@ -1,26 +1,49 @@
 class Com_Handler {
-	constructor(com){
+	constructor(com,ws){
 		this.com = com;
+		this.ws = ws;
+		this.setup()
 	}
 	playerConnection(pinfos,resume){
 		if (resume==0){
-			infos = this.getInfo();
-			player = this.com.pc_app.campaign.addPlayer(infos,this);
-			this.player = player;
+			this.getInfo();
 		}
 		else {
-			player = this.getName()
+			this.getName(pinfos);
 			
 		}
 	}
 
 	getInfo() {
-		//On récupère la trame d'informations de l'appli téléphone
-
-
-
-		return infos;
+		ws.send(JSON.stringify("type" : "new","data": NULL)) //On envoie une demande d'infos de nouveau joueur
 	}
 
+	getName(pinfos) {
+		ws.send(JSON.stringify("type" : "choice","data": pinfos))
+	}
+
+	setup(){
+		this.ws.onmessage = (event) => { //Fonction de récéption de message entrant
+			try {
+        		var obj = JSON.parse(event.data) //On essaye de récupérer notre trame
+     		} catch (e) {
+        		console.log(e); //On affiche l'erreur si la trame n'a pas pu être récupérée
+      			}
+
+
+		}
+		if(obj.type == "newrep"){ //Le joueur a envoyé ses informations
+			infos = obj.data; //On enregistre les infos
+			this.com.pc_app.campaign.addPlayer(infos,this);
+
+		}
+		else if(obj.type == "choicerep"){ //Si le joueur a choisi son nom
+			name = obj.data; //On enregistre le nom
+			this.com.pc_app.campaign.resumePlayer(name,this); //On apelle resumeplayer de campaign avec le nom et cet objet
+		}
+		else { //On veut sauvegarder le joueur
+
+		}
+	}
 
 }
